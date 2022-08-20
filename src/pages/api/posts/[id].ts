4b1b7f0ef6805b5ddf2a5a84fad947e68092b567
Josep Galearez <51718252+galearez@users.noreply.github.com>
2022-08-19 18:53:07 -0600

    create put route

--- src/pages/api/posts/[id].ts
+++ src/pages/api/posts/[id].ts
@@ -15,4 +15,35 @@ async function get({ params }: APIContext) {
   }
 }
 
-export { get };
+async function put({ params, request }: APIContext) {
+  const body = await request.formData();
+  let urlQueries: any = {};
+  for (const query of body) {
+    urlQueries = { ...urlQueries, [query[0]]: query[1] };
+  }
+
+  if (
+    !Object.hasOwn(urlQueries, 'title') ||
+    !Object.hasOwn(urlQueries, 'body')
+  ) {
+    return Response.redirect('http://localhost:3000/new', 304);
+  }
+
+  if (!Object.hasOwn(urlQueries, 'id') && urlQueries.id !== params.id) {
+    return Response.redirect('http://localhost:3000/new', 304);
+  }
+
+  const query = {
+    text: 'UPDATE posts SET post_title = $2, post_body = $3 WHERE post_id = $1',
+    values: [params.id, urlQueries.title, urlQueries.body] as string[],
+  };
+
+  try {
+    const res = await getPool().then((res) => res.query(query));
+    return Response.redirect(`http://localhost:3000/p/${params.id}`, 301);
+  } catch {
+    return new Response('CANNOT UPDATE', { status: 500 });
+  }
+}
+
+export { get, put };
